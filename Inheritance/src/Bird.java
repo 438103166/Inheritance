1a50b60d07d2c07a6f04ea579c52ba91513fba0e
sryaa <sryaa@DESKTOP-KQ5V2P2>
2021-03-18 11:42:53 +0300

    F

---
+++ Inheritance/src/Bird.java
@@ -0,0 +1,9 @@
+public class Bird {    
+	String color="RED";
+	public static void fly(){
+        System.out.println("Bird is flying");
+    }
+    public void tweet(){
+        System.out.println("tweet");
+    }
+}
---
+++ Inheritance/src/Parrot.java
@@ -0,0 +1,18 @@
+
+public class Parrot extends Bird{
+public static void main(String[] args) {
+	Parrot parrot = new Parrot();
+    parrot.fly();
+    
+		// TODO Auto-generated constructor stub
+	} 
+public void talk(String word){
+        System.out.println(word);
+}
+
+	
+
+}
+
+
+
